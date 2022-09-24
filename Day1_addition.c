25dbd399409f0aa987888e913a10797f69f1f3ef
MURUGANANTHAM07 <113437752+MURUGANANTHAM07@users.noreply.github.com>
2022-09-24 10:00:52 -0700

    Create Day1_addition.c

---
+++ Day1_addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the number a=");
+    scanf("%d",&a);
+    printf("enter the number b=");
+    scanf("%d",&b);
+    {
+        c=a+b;
+        printf("%d",c);
+    }
+    return 0;
+}
